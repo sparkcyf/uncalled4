111312beccb77cfb0d0998937bc89c7f34d9ee18
sparkcyf <sparktour@outlook.com>
2024-12-26 17:20:04 +0800

    use groupby to accelerate kmer extraction process

--- src/uncalled4/io/model_trainer.py
+++ src/uncalled4/io/model_trainer.py
@@ -145,98 +145,111 @@ class ModelTrainer(TrackIO):
     def is_full(self):
         return self.model is not None and len(self.full_kmers) == self.model.KMER_COUNT
 
+
     def next_model(self, load_index=False):
         self.close()
         self.init_read_mode(load_index=load_index)
-
         self.kmer_index.sort_index(inplace=True)
-
+        
         t = time()
-        model_rows = list()
-        for kmer in self.kmer_index.index.unique():
-            chunks = self.kmer_index.loc[[kmer]]
-            rows = np.zeros(chunks["length"].sum(), dtype=self.row_dtype)
+        # 预分配大数组存储所有数据
+        total_length = self.kmer_index['length'].sum()
+        all_rows = np.zeros(total_length, dtype=self.row_dtype)
+        
+        # 一次性读取所有数据
+        offset = 0
+        starts = self.kmer_index['start'].values
+        lengths = self.kmer_index['length'].values
+        for start, length in zip(starts, lengths):
+            self.input.seek(start * self.itemsize)
+            all_rows[offset:offset + length] = np.fromfile(self.input, self.row_dtype, length)
+            offset += length
+        
+        # 使用pandas的groupby进行快速统计
+        df = pd.DataFrame(all_rows)
+        if self.tprms.train_mean:
+            agg_func = 'mean'
+        else:
+            agg_func = 'median'
 
-            i = 0
-            for _,(start,length) in chunks.iterrows():
-                self.input.seek(start*self.itemsize)
-                rows[i:i+length] = np.fromfile(self.input, self.row_dtype, length)
-                i += length
+        print("read done!")
+            
+        # 一次性计算所有统计量
+        stats = df.groupby('kmer').agg({
+            'current': [agg_func, 'std'],
+            'current_sd': [agg_func, 'std'],
+            'dwell': [agg_func, 'std'],
+        })
 
-            if self.tprms.train_mean:
-                avg = np.mean
-            else:
-                avg = np.median
-
-            def filt(a):
-                return a
-
-            current = filt(rows["current"])
-            current_sd = filt(rows["current_sd"])
-            dwell = filt(rows["dwell"])
-
-            k = self.model.kmer_to_str(kmer)
-
-            model_rows.append((
-                kmer,
-                avg(current),
-                avg(current_sd),
-                avg(dwell),
-                np.std(rows["current"]),
-                np.std(rows["current_sd"]),
-                np.std(rows["dwell"]),
-                len(rows)
-            ))
-
-        df = pd.DataFrame(model_rows, columns=["kmer", "current.mean", "current_sd.mean", "dwell.mean", "current.stdv", "current_sd.stdv", "dwell.stdv", "count"])
-
-        subs_locs = np.array([0,self.model.K-1])
-
-        prms = PoreModelParams(self.model.PRMS)
-
+        print("group done!")
+        
+        # 重组数据为所需格式，保持kmer作为列
+        model_df = pd.DataFrame()
+        model_df['kmer'] = stats.index
+        model_df.set_index('kmer', inplace=True)
+        model_df['current.mean'] = stats['current'][agg_func]
+        model_df['current_sd.mean'] = stats['current_sd'][agg_func]
+        model_df['dwell.mean'] = stats['dwell'][agg_func]
+        model_df['current.stdv'] = stats['current']['std']
+        model_df['current_sd.stdv'] = stats['current_sd']['std']
+        model_df['dwell.stdv'] = stats['dwell']['std']
+        model_df['count'] = df.groupby('kmer').size()
+        
+        # 确保包含所有KMERS
+        model_df = model_df.reindex(self.model.KMERS)
+        
+        # 处理missing values
         if self.conf.train.init_mode == "moves_avg" and self.conf.dtw.norm_iterations == 0:
             bases = self.conf.train.moves_avg
             if bases is None:
                 bases = [self.model.shift]
 
             for b in bases:
-                df[b] = self.model.kmer_base(self.model.KMERS, b)
+                model_df[b] = self.model.kmer_base(self.model.KMERS, b)
+                
+            base_groups = model_df.groupby(bases)
+            model_df['current.mean'] = base_groups['current.mean'].transform('median')
+            model_df['current.stdv'] = base_groups['current.stdv'].transform('std')
+            model_df['count'] = base_groups['count'].transform('sum')
 
-            grp = df.groupby(bases)["current.mean"]
-
-            df = pd.DataFrame(df.set_index(bases)["kmer"])
-            df["current.mean"] = grp.median().loc[df.index]
-            df["current.stdv"] = grp.std().loc[df.index]
-            df["count"] = grp.count().loc[df.index]
-
-        df = df.set_index("kmer", drop=True).reindex(self.model.KMERS)
-
-        for kmer in df.index[df["current.mean"].isna()]:
-            subs = list()
-            for i in subs_locs:
-                old = self.model.kmer_base(kmer, i)
-                subs.append(self.model.set_kmer_base(kmer, i, [b for b in range(4) if b != old]))
-            subs = np.unique(np.concatenate(subs))
-            means = df.loc[subs, "current.mean"].dropna().sort_values()
-            if len(means) > 0:
-                mid = means.index[len(means)//2]
-                df.loc[kmer] = df.loc[mid]
-            else:
-                df.loc[kmer] = self.model.to_df().loc[kmer]
-            df.loc[kmer, "count"] = 0
-
+        # 处理缺失的kmer
+        subs_locs = np.array([0, self.model.K-1])
+        missing_kmers = model_df.index[model_df['current.mean'].isna()]
+        
+        if len(missing_kmers) > 0:
+            for kmer in missing_kmers:
+                subs = []
+                for i in subs_locs:
+                    old = self.model.kmer_base(kmer, i)
+                    subs.extend(self.model.set_kmer_base(kmer, i, [b for b in range(4) if b != old]))
+                subs = np.unique(subs)
+                means = model_df.loc[subs, 'current.mean'].dropna().sort_values()
+                
+                if len(means) > 0:
+                    mid = means.index[len(means)//2]
+                    model_df.loc[kmer] = model_df.loc[mid]
+                else:
+                    model_df.loc[kmer] = self.model.to_df().loc[kmer]
+                model_df.loc[kmer, 'count'] = 0
+        
+        # 保存模型
         outfile = self._filename("model.npz")
         self.model.PRMS.name = outfile
-        rev = prms.reverse
-        model_out = PoreModel(model=df, k=prms.k, extra_cols=True)
+        prms = PoreModelParams(self.model.PRMS)
+        
+        print("save done!")
+        
+        # 重置索引，确保kmer作为列传入PoreModel
+        model_df = model_df.reset_index()
+        model_out = PoreModel(model=model_df, k=prms.k, extra_cols=True)
         model_out.PRMS = self.model.PRMS
         model_out.to_npz(outfile)
-
-        self.set_model(PoreModel(params=self.model.PRMS, extra_cols=True))
-
-        self.iter += 1
-        return self.model 
         
+        self.set_model(PoreModel(params=self.model.PRMS, extra_cols=True))
+        self.iter += 1
+        
+        return self.model
+
 
     def close(self):
         if not self.prms.buffered:
